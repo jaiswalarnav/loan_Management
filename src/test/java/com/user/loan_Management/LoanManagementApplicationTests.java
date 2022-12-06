783f207d6a30f60d64c02c0265a61dbea2eec20a
jaiswalarnav <jaiswalarnav019@gmail.com>
2022-12-06 16:17:36 +0530

    my first commit

---
+++ src/test/java/com/user/loan_Management/LoanManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.loan_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
