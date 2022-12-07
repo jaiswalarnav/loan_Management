dd17f0cfa44fdc6715c6b37e9cd9b04bc296d817
jaiswalarnav <jaiswalarnav019@gmail.com>
2022-12-07 19:26:24 +0530

    added primary key

--- src/main/java/com/user/loan_Management/model/Pan.java
+++ src/main/java/com/user/loan_Management/model/Pan.java
@@ -5,10 +5,15 @@ import java.sql.Timestamp;
 import com.user.loan_Management.model.support.Support;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Pan extends Support{
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String panNo;
 	private String name;
